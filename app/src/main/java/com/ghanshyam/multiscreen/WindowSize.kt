ea788478f975a62abace42440380f2973490987c
Ghanshyam32 <ghanshyammishra3205615@gmail.com>
2024-04-18 23:30:48 +0530

    first commit

---
+++ app/src/main/java/com/ghanshyam/multiscreen/WindowSize.kt
@@ -0,0 +1,30 @@
+package com.ghanshyam.multiscreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+
+data class WindowSize(
+    val width: WindowType,
+    val height: WindowType
+)
+
+enum class WindowType {
+    Compact, Medium, Expanded
+}
+
+@Composable
+fun rememberWindowSize(): WindowSize {
+    val configuration = LocalConfiguration.current
+    return WindowSize(
+        width = when {
+            configuration.screenWidthDp < 600 -> WindowType.Compact
+            configuration.screenWidthDp < 840 -> WindowType.Medium
+            else -> WindowType.Expanded
+        },
+        height = when {
+            configuration.screenHeightDp < 480 -> WindowType.Compact
+            configuration.screenHeightDp < 900 -> WindowType.Medium
+            else -> WindowType.Expanded
+        }
+    )
+}
